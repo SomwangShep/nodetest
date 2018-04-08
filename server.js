bed0a246dfd79ae0fca1bd2b4526969ce1b039a6
SomwangShep <ss1792@mcla.edu>
2018-04-07 20:27:44 -0400

    Added java execute

--- server.js
+++ server.js
@@ -1,55 +1,105 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const yargs = require('yargs');
 const port = process.env.PORT || 3000;
 
+// const argv = yargs.argv;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-
-app.use((req,res,next)=>{
+// ============== Create the log file =================
+app.use((req,res,next)=>
+{
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
   console.log(log);
-  fs.appendFile('server.log',log + '\n', (err)=>{
-    if (err){
-      console.log('Unable to append to server.log')}
+  fs.appendFile('server.log',log + '\n', (err)=>
+  {
+    if (err)
+    {
+      console.log('Unable to append to server.log')
+    }
   });
   next();
 });
-
-// app.use((req, res, next)=>{
-//   res.render('maintenance.hbs');
-// });
+// ============== End Create the log file ===============
 
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', () => {
+hbs.registerHelper('getCurrentYear', () =>
+{
   return new Date().getFullYear()
 });
 
-hbs.registerHelper('screamIt',(text)=>{
+hbs.registerHelper('screamIt',(text)=>
+{
   return text.toUpperCase();
 });
 
-app.get('/', (req, res)=> {
-  res.render('home.hbs',{
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
-    // currentYear: new Date().getFullYear()
+app.get('/', (req, res)=>
+{
+  // =============== Start execute java file =========================//
+  var fn = __dirname + '/public/GenerateQuiz1Question.jar';
+  var spawn = require('child-process-promise').spawn;
+  var promise = spawn('java', ['-jar',fn]);
+  var childProcess = promise.childProcess;
+
+  var q = "";//return frim java
+  var err = "";//error from childProcess
+  var status = "";//results frpom promise
+
+  childProcess.stdout.on('data', function (data)
+  {
+    q = data.toString();
+    // str.replace("Microsoft", "W3Schools");
+    // q = q.replace(";","</br>");
+    console.log('Yargs', yargs.argv);
+    console.log('[spawn] stdout: ', q);
+    // console.log('here is questions in stdout:  '+q);
+    res.render('home.hbs',
+    {
+      pageTitle: 'Home Page',
+      welcomeMessage: 'Welcome to my website',
+      quetion: `${q}`,
+      error: `${err}`,
+      status: `${status}`
+    });
+    // quetion: `${q}`,
+    // console.log('here is questions in render:  '+q);
   });
 
-  // res.send('<h1>Hellow express</h1>');
+  // childProcess.stderr.on('data', function (data)
+  // {
+  //   err = data.toString();
+  //   console.log('[spawn] stderr: ', err);
+  //   console.log('here is questions in stderr:  '+q);
+  // });
 
-  // res.send({
-  //   name: 'Somwang',
-  //   like: [
-  //     'aple',
-  //     'orange'
-  //   ]
-  //   });
+  // promise.then(function ()
+  // {
+  //   status = "Complete!";
+  //   console.log(status);
+  // })
+  // .catch(function (err)
+  // {
+  //   status = err;
+  //   console.error('[spawn] ERROR: ', err);
+  // });
+
+  console.log('here is questions before render:  '+q);
+  // =============== End execute java file =========================//
+
+//   res.render('home.hbs',{
+//     pageTitle: 'Home Page',
+//     welcomeMessage: 'Welcome to my website',
+//     quetion: `${q}`,
+//     error: `${err}`,
+//     status: `${status}`
+//   });
+//   console.log('here is questions in render:  '+q);
 });
 
 app.get('/about',(req,res) => {
@@ -57,13 +107,6 @@ app.get('/about',(req,res) => {
   res.render('about.hbs',{
     pageTitle: 'About Page2',
     // currentYear: new Date().getFullYear()
-  });
-});
-
-app.get('/bad',(req,res) => {
-  res.send({
-    error: 'bad data',
-    detail: 'this is bad page'
   });
 });
 
